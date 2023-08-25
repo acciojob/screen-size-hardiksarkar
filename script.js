1b5cffd28d9b46528c03f1a8346ccce8587626e1
Hardik Sarkar <107950304+hardiksarkar@users.noreply.github.com>
2023-08-25 09:18:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let div = document.getElementById("sizeinfo");
+let h1 = div.firstElementChild;
+
+window.addEventListener("resize",()=>{
+     h1.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`
+})
+h1.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`
